2b74d2dbd9423f144f2f7aec55f72c8d48a9e44f
medhaouari7 <medhaouari025@gmail.com>
2022-05-27 12:29:05 +0100

    header-3

--- kfc-app/frontend/src/components/Card/index.jsx
+++ kfc-app/frontend/src/components/Card/index.jsx
@@ -1,19 +1,22 @@
 import React from "react";
+import product1 from '../../assets/img/product1.png'
+import './card.css';
 
 const Card = () => {
   return (
     <div className="card-wrapper">
       <div class="card">
-        <img src="..." class="card-img-top" alt="..." />
+        <img src={product1} class="card-img-top" alt="..." />
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <p class="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </p>
-          <a href="#" class="btn btn-primary">
-            Go somewhere
-          </a>
+            <div className="btn-wrapper d-flex">
+            <button type="button" class="btn btn-outline-danger btn-wrapper">200 $</button>
+            <button href="button" class="btn btn-danger btn-wrapper">Add to Bucket</button>
+            </div>
         </div>
       </div>
     </div>
