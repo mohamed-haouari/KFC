f7a5ab2f9557c158214ff84bdaddf81f3f45596e
medhaouari7 <medhaouari025@gmail.com>
2022-05-27 11:47:10 +0100

    header-2

--- kfc-app/frontend/src/components/Header/index.jsx
+++ kfc-app/frontend/src/components/Header/index.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import logo from '../../assets/img/vector.svg';
+import './header.css';
+import Menu from '../Menu';
 
 const Header = () => {
   return (
-    <div className="headerWrapper">
+<>
+<div className="headerWrapper">
         <div className="logo">
             <img src={logo} alt="" />
         </div>
         <div className="auth-link">
     <ul>
-        <li><a href="">Store Locator</a></li>
-        <li><a href="">Signin/Register</a></li>
+        <li><a href=""><i class="bi bi-geo-alt-fill"></i>Store Locator</a></li>
+        <li><a href=""><i class="bi bi-person-fill"></i>Signin/Register</a></li>
     </ul>
         </div>
     </div>
+    <Menu/>
+    </>
   )
 }
 
