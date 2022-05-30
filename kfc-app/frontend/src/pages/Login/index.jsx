99da0c124d136841f38c43f948a014d528167d4b
medhaouari7 <medhaouari025@gmail.com>
2022-05-30 14:21:11 +0100

    Login Validation

--- kfc-app/frontend/src/pages/Login/index.jsx
+++ kfc-app/frontend/src/pages/Login/index.jsx
@@ -2,16 +2,37 @@ import React from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import { Link } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const Login = () => {
+  const { handleSubmit, handleChange, handleBlur, values, touched, errors } =
+    useFormik({
+      initialValues: {
+        email: "",
+        password: "",
+      },
+      validationSchema: Yup.object({
+        email: Yup.string()
+          .email("Please provide a valid email")
+          .required("Email is Required"),
+        password: Yup.string()
+          .min(6, "Must be 6 characters or more")
+          .required("Password is Required"),
+      }),
+      onSubmit: (values) => {
+        console.log(values);
+      },
+    });
+
   return (
     <>
       <Header />
       <div className="login-wrapper">
         <div className="row">
           <div className="col-md-6">
-          <div className="lead-text">
-            <h3>LOGIN WITH SOCIAL LINKS</h3>
+            <div className="lead-text">
+              <h3>LOGIN WITH SOCIAL LINKS</h3>
             </div>
             <p>
               Don't have Account?<Link to="/register">Register</Link>
@@ -24,36 +45,56 @@ const Login = () => {
               Login with Facebook
             </button>
             <hr />
-            <form className="row g-3">
+            <form className="row g-3" onSubmit={handleSubmit}>
               <div className="col-md-12">
-                <label htmlFor="inputEmail4" className="form-label">
+                <label htmlFor="email" className="form-label">
                   Email
                 </label>
-                <input type="email" className="form-control" id="inputEmail4" />
+                <input
+                  type="email"
+                  onChange={handleChange}
+                  value={values.email}
+                  onBlur={handleBlur}
+                  name="email"
+                  className="form-control"
+                  id="email"
+                />
+                <p className="error">
+                  {touched.email && errors.email ? errors.email : null}
+                </p>
               </div>
               <div className="col-md-12">
-                <label htmlFor="inputPassword4" className="form-label">
+                <label htmlFor="password" className="form-label">
                   Password
                 </label>
                 <input
                   type="password"
+                  name="password"
                   className="form-control"
-                  id="inputPassword4"
+                  id="password"
+                  onChange={handleChange}
+                  value={values.password}
+                  onBlur={handleBlur}
                 />
+                <p className="error">
+                  {touched.password && errors.password ? errors.password : null}
+                </p>
               </div>
               <div className="col-12">
                 <button type="submit" className="btn btn-primary">
-                Login Now
+                  Login Now
                 </button>
               </div>
             </form>
           </div>
           <div className="col-md-6">
-          <div className="lead-text">
-            <h3>NEW CUSTOMER</h3>
+            <div className="lead-text">
+              <h3>NEW CUSTOMER</h3>
             </div>
             <p>Register please to enjoy our services.</p>
-            <Link to="/register" class="btn btn-outline-danger">NEW CUSTOMER</Link>
+            <Link to="/register" class="btn btn-outline-danger">
+              NEW CUSTOMER
+            </Link>
           </div>
         </div>
       </div>
--- kfc-app/frontend/src/pages/Register/index.jsx
+++ kfc-app/frontend/src/pages/Register/index.jsx
@@ -22,7 +22,7 @@ const Register = () => {
       firstName: Yup.string().max(15, 'Must be 15 characters or less').required('First Name Required'),
       lastName:  Yup.string().max(15, 'Must be 15 characters or less').required('Last Name Required'),
       email: Yup.string().email('Invalid Email Adress').required('Email is Required'),
-      password : Yup.string().min(6, 'Must be 6 characters or more').required('Password Required'),
+      password : Yup.string().min(6, 'Must be 6 characters or more').required('Password is Required'),
       adress : Yup.string().required('Adress is Required'),
       city: Yup.string().required('City is Required'),
       state: Yup.string().required('State is Required'),
@@ -49,32 +49,32 @@ const Register = () => {
   <div className="col-md-6">
     <label htmlFor="firstName" className="form-label">First Name</label>
     <input type="text" onChange={handleChange} value={values.firstName} onBlur={handleBlur} name="firstName" className="form-control" id="firstName"/>
-    <p className='error'>{errors.firstName}</p>
+    <p className='error'>{ touched.firstName && errors.firstName ? errors.firstName:null }</p>
   </div>
   <div className="col-md-6">
     <label htmlFor="lastName" className="form-label">Last Name</label>
     <input type="text" onChange={handleChange} value={values.lastName} onBlur={handleBlur} name="lastName" className="form-control" id="lastName"/>
-    <p className='error'>{errors.lastName}</p>
+    <p className='error'>{ touched.lastName && errors.lastName ? errors.lastName:null }</p>
   </div>
   <div className="col-12">
     <label htmlFor="email" className="form-label">Email</label>
     <input type="email" onChange={handleChange} value={values.email} onBlur={handleBlur} name="email" className="form-control" id="email"/>
-    <p className='error'>{errors.email}</p>
+    <p className='error'>{ touched.email && errors.email ? errors.email:null }</p>
   </div>
   <div className="col-12">
     <label htmlFor="password" className="form-label">Password</label>
     <input type="password" onChange={handleChange} value={values.password} onBlur={handleBlur} name="password" className="form-control" id="password"/>
-    <p className='error'>{errors.password}</p>
+    <p className='error'>{ touched.password && errors.password ? errors.password:null }</p>
   </div>
   <div className="col-md-6">
     <label htmlFor="adress" className="form-label">Adress</label>
     <input type="text" name="adress" onChange={handleChange} value={values.adress} onBlur={handleBlur} className="form-control" id="adress"/>
-    <p className='error'>{errors.adress}</p>
+    <p className='error'>{ touched.adress && errors.adress ? errors.adress:null }</p>
   </div>
   <div className="col-md-6">
     <label htmlFor="city" className="form-label">City</label>
     <input type="text" onChange={handleChange} value={values.city} onBlur={handleBlur} name="city" className="form-control" id="city"/>
-    <p className='error'>{errors.adress}</p>
+    <p className='error'>{ touched.city && errors.city ? errors.city:null }</p>
   </div>
   <div className="col-md-4">
     <label htmlFor="state" className="form-label">State</label>
@@ -85,12 +85,12 @@ const Register = () => {
       <option>Chicago</option>
       <option>California</option>
     </select>
-    <p className='error'>{errors.state}</p>
+    <p className='error'>{ touched.state && errors.state ? errors.state:null }</p>
   </div>
   <div className="col-md-2">
     <label htmlFor="zip" className="form-label">Zip</label>
     <input type="text" onChange={handleChange} value={values.zip} onBlur={handleBlur} name="zip" className="form-control" id="zip"/>
-    <p className='error'>{errors.zip}</p>
+    <p className='error'>{ touched.zip && errors.zip ? errors.zip:null }</p>
   </div>
   <div className="col-12">
     <button type="submit" className="btn btn-primary">Register Now</button>
