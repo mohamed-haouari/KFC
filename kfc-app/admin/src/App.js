0485e849af131edcef951a3be9dd502c6d14be49
medhaouari7 <medhaouari025@gmail.com>
2022-05-31 14:17:34 +0100

    Admin-Login

--- kfc-app/admin/src/App.js
+++ kfc-app/admin/src/App.js
@@ -2,17 +2,19 @@ import './App.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
+import Login from './pages/Login';
 
 function App() {
   return (
     <>
-      <Header/>
+      {/* <Header/>
       <div class="container-fluid">
       <div class="row">
         <Navbar/>
         <Main/>
       </div>
-      </div>
+      </div> */}
+      <Login/>
     </>
   );
 }
